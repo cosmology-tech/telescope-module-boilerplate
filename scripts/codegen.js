aaaf697e97cbf30213bbf09b0b5884ae813fcbc9
Zetazzz <zetazz@163.com>
2023-09-15 04:59:51 +0800

    remove some excluded pkgs

--- scripts/codegen.js
+++ scripts/codegen.js
@@ -28,33 +28,28 @@ telescope({
       },
       excluded: {
         packages: [
-          // 'ibc.applications.fee.v1',
+          'ibc.applications.fee.v1', // issue with parsing protos (LCD routes with nested objects in params)
 
           'cosmos.app.v1alpha1',
           'cosmos.app.v1beta1',
-          'cosmos.autocli.v1',
           'cosmos.base.kv.v1beta1',
           'cosmos.base.reflection.v1beta1',
           'cosmos.base.snapshots.v1beta1',
           'cosmos.base.store.v1beta1',
           'cosmos.base.tendermint.v1beta1',
-          'cosmos.capability.v1beta1',
           'cosmos.crisis.v1beta1',
           'cosmos.evidence.v1beta1',
-          'cosmos.feegrant.v1beta1',
           'cosmos.genutil.v1beta1',
-          'cosmos.gov.v1',
-          'cosmos.group.v1',
-          'cosmos.group.v1beta1',
-          'cosmos.mint.v1beta1',
+
+          'cosmos.autocli.v1',
+
           'cosmos.msg.v1',
           'cosmos.nft.v1beta1',
-          'cosmos.orm.v1',
+          'cosmos.capability.v1beta1',
           'cosmos.orm.v1alpha1',
-          'cosmos.params.v1beta1',
+          'cosmos.orm.v1',
           'cosmos.slashing.v1beta1',
-          'cosmos.vesting.v1beta1',
-          // 'google.api',
+          'google.api',
           'ibc.core.port.v1',
           'ibc.core.types.v1'
         ]
